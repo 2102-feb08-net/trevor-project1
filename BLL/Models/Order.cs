21ddcce420c0cbe14ad508249bad2313014635ff
Trevor Dunbar <tdunbar123@yahoo.com>
2021-02-27 16:00:11 -0500

    Finished BLL implementation

--- BLL/Models/Order.cs
+++ BLL/Models/Order.cs
@@ -10,7 +10,7 @@ namespace BLL.Models
     {
         private decimal _totalPrice;
 
-        public Dictionary<Product, int> Items { get; set; }
+        public Dictionary<Product, int> Cart { get; set; }
 
         public int ID { get; set; }
         public decimal TotalPrice
@@ -38,10 +38,104 @@ namespace BLL.Models
 
         public Order(Customer customer, Store store)
         {
-            Items = new Dictionary<Product, int>();
+            Cart = new Dictionary<Product, int>();
             TotalPrice = 0.0M;
             Customer = customer;
             Store = store;
+        }
+
+        public void AddNewItemToCart(Product product, int quantity)
+        {
+            if (Cart.ContainsKey(product))
+            {
+                throw new ArgumentException("Item already exists in the inventory");
+            }
+            else if (quantity <= 0)
+            {
+                throw new ArgumentException("Can't add new item with 0 or less than 0 quantity to inventory");
+            }
+            else if (!Store.Inventory.ContainsKey(product))
+            {
+                throw new ArgumentException("Can't add item to cart because it doesn't exist in store inventory");
+            }
+            else if(quantity > Store.Inventory[product])
+            {
+                throw new ArgumentException("Can't add item to cart, not enough inventory available");
+            }
+            else
+            {
+                Cart[product] = quantity;
+                Store.Inventory[product] -= quantity;
+            }
+        }
+
+        public void AddQuantityToCart(Product product, int quantity)
+        {
+            if (!Cart.ContainsKey(product))
+            {
+                throw new ArgumentException("Can't add quantity because item doesn't exist in inventory");
+            }
+            else if (quantity <= 0)
+            {
+                throw new ArgumentException("Can't add 0 or less than 0 quantity to item in inventory");
+            }
+            else if (!Store.Inventory.ContainsKey(product))
+            {
+                throw new ArgumentException("Can't add quantity for item in cart because it doesn't exist in the store");
+            }
+            else if(quantity > Store.Inventory[product])
+            {
+                throw new ArgumentException("Can't add quantity for item in cart because not enough store inventory available");
+            }
+            else
+            {
+                Cart[product] += quantity;
+                Store.Inventory[product] -= quantity;
+            }
+        }
+
+        public void RemoveQuantityFromCart(Product product, int quantity)
+        {
+            if (!Cart.ContainsKey(product))
+            {
+                throw new ArgumentException("Can't remove quantity because item doesn't exist in inventory");
+            }
+            else if (quantity <= 0)
+            {
+                throw new ArgumentException("Can't remove 0 or less than 0 quantity from item in inventory");
+            }
+            else if (quantity > Cart[product])
+            {
+                throw new ArgumentException("Can't remove more than what already exists for item in inventory");
+            }
+            else
+            {
+                Cart[product] -= quantity;
+                Store.Inventory[product] += quantity;
+            }
+        }
+
+        public void DeleteProductFromCart(Product product)
+        {
+            if (!Cart.ContainsKey(product))
+            {
+                throw new ArgumentException("Can't delete product from inventory because it doesn't exist");
+            }
+            else
+            {
+                Store.Inventory[product] += Cart[product];
+                Cart.Remove(product);
+            }
+        }
+
+        public void SubmitOrder()
+        {
+            foreach(var item in Cart)
+            {
+                TotalPrice += item.Key.Price * item.Value;
+            }
+            Store.GrossProfit += TotalPrice;
+            OrderTime = DateTime.Now;
         }
     }
 }
--- BLL/Models/Store.cs
+++ BLL/Models/Store.cs
@@ -60,5 +60,69 @@ namespace BLL.Models
             GrossProfit = 0.0M;
         }
 
+        public void AddNewItemToInventory(Product product, int quantity)
+        {
+            if (Inventory.ContainsKey(product))
+            {
+                throw new ArgumentException("Item already exists in the inventory");
+            }
+            else if(quantity <= 0)
+            {
+                throw new ArgumentException("Can't add new item with 0 or less than 0 quantity to inventory");
+            }
+            else
+            {
+                Inventory[product] = quantity;
+            }
+        }
+
+        public void AddQuantityToItem(Product product, int quantity)
+        {
+            if (!Inventory.ContainsKey(product))
+            {
+                throw new ArgumentException("Can't add quantity because item doesn't exist in inventory");
+            }
+            else if(quantity <= 0)
+            {
+                throw new ArgumentException("Can't add 0 or less than 0 quantity to item in inventory");
+            }
+            else
+            {
+                Inventory[product] += quantity;
+            }
+        }
+
+        public void RemoveQuantityFromItem(Product product, int quantity)
+        {
+            if (!Inventory.ContainsKey(product))
+            {
+                throw new ArgumentException("Can't remove quantity because item doesn't exist in inventory");
+            }
+            else if (quantity <= 0)
+            {
+                throw new ArgumentException("Can't remove 0 or less than 0 quantity from item in inventory");
+            }
+            else if(quantity > Inventory[product])
+            {
+                throw new ArgumentException("Can't remove more than what already exists for item in inventory");
+            }
+            else
+            {
+                Inventory[product] -= quantity;
+            }
+        }
+
+        public void DeleteProductFromInventory(Product product)
+        {
+            if (!Inventory.ContainsKey(product))
+            {
+                throw new ArgumentException("Can't delete product from inventory because it doesn't exist");
+            }
+            else
+            {
+                Inventory.Remove(product);
+            }
+        }
+
     }
 }
