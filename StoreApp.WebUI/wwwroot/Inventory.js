1fc40855d81379e91fd22472dc6ecf79395b6a74
Trevor Dunbar <tdunbar123@yahoo.com>
2021-03-04 00:37:10 -0500

    Added "add item" and "edit item" forms

--- StoreApp.WebUI/wwwroot/Inventory.js
+++ StoreApp.WebUI/wwwroot/Inventory.js
@@ -1,4 +1,6 @@
 ﻿const inventoryTable = document.getElementById("inventory-table");
+const editDropdown = document.getElementById("product-edit-select");
+const editForm = document.getElementById("edit-item-form");
 const errorMessage = document.getElementById("error-message");
 const storeID = sessionStorage.getItem("storeId");
 
@@ -15,12 +17,17 @@ loadInventory(storeID)
     .then(inventory => {
         for (const item of inventory) {
             const row = inventoryTable.insertRow();
+            const option = document.createElement("option");
             row.innerHTML = `<td>${item.id}</td>
                        <td>${item.name}</td>
-                       <td>${item.price}</td>
+                       <td>\$${item.price}</td>
                        <td>${item.quantity}</td>`;
+            option.value = item.id;
+            option.text = item.name;
+            editDropdown.appendChild(option);
         }
         inventoryTable.hidden = false;
+        editForm.hidden = false;
     })
     .catch(error => {
         errorMessage.textContent = error.toString();
